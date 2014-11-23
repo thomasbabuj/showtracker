b06178443c57425666372dd7ee2d0335e37bf414
Thomas Babu <tommy.developer@gmail.com>
2014-11-23 17:27:33 +0800

    connect to the db

--- server.js
+++ server.js
@@ -60,6 +60,8 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
+mongoose.connect('localhost');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
